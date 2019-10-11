5138dd08e490c4b97ea61d113c959085e5d02469
s3rbane <jesse@serbane.com>
2019-10-11 15:57:21 -0400

    added a windows auto login script written in Powershell

---
+++ WindowsAutoLogin/createAutoLogin.ps1
@@ -0,0 +1,23 @@
+﻿#enter your domain below, leave blank if user is not a member of a domain
+$domain = ''
+$autoLoginUserName = Read-Host -Prompt 'Input the Username (no domain)'
+$autoLoginPassword = Read-Host -Prompt 'Input the Users Password'
+
+#run if the properties exist, ex. the user is being changed, it ran and failed previously, etc.
+try{
+Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "DefaultDomainName"
+Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "DefaultUserName"
+Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "DefaultPassword"
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultDomainName -Value $domain
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultUserName -Value $autoLoginUserName
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultPassword -Value $autoLoginPassword
+}
+#instead of properly checking if the values exist, if they don't, we simply add them in the error handling block below if the above block throws an error (because it wont have any values to remove).
+catch{
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultDomainName -Value $domain
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultUserName -Value $autoLoginUserName
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultPassword -Value $autoLoginPassword
+Set-Itemproperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoAdminLogon -value '1'
+}
+
+#}
