6692b2ed78a3ec6537c283048118597e3a1f7158
Zachery Massey <masseyzachery57@yahoo.com>
2019-10-13 21:40:26 -0400

    Added minecraft autolauncher

---
+++ minecraft_autolauncher/README.md
@@ -0,0 +1,4 @@
+Minecraft Auto-launching
+
+basically it's a way to start minecraft and hit the play button on the login screen for those who 
+wish to click twice with one click.
---
+++ minecraft_autolauncher/minecraft_autolauncher.py
@@ -0,0 +1,11 @@
+#it's a simple script that auto opens minecraft but it's quite effective
+import pyautogui 
+
+pyautogui.click(x=143,y=1059)
+
+pyautogui.typewrite('minecraft')
+pyautogui.press('enter')
+
+pyautogui.moveTo(1046,989,10)
+pyautogui.click()
+
