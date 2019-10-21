420e872ff5f530ce1f59c395a858e4983ba95af0
iwontheboot <wlee11@nd.edu>
2019-10-20 21:35:47 -0400

    WORKSgit add check_dup.py  While not walking through the directories

--- check_dup/check_dup.py
+++ check_dup/check_dup.py
@@ -1,13 +1,49 @@
 #!/usr/bin/env python3
 
 import os
+import subprocess
 	
 # diff: 0 if same, 1 if different, 2 if trouble
 
 # Global list
 dontCheck = set()
 
+def checkPrompt(fiFo, path):
+	# If duplicates found, notify that duplicates have been found
+	print()
+	print("Duplicates found for {}.".format(path))
+
+	# Display all the duplicates of that file in form (1: ... \n 2: ... etc.)
+	for key, fi in enumerate(fiFo):
+		print("{}. {}".format(key + 1, fi))
+
+	# Prompt if they would like to keep all files
+	keepAll = "X"
+	while keepAll != "y" and keepAll != "n":
+		keepAll = input("Would you like to keep all the above files? (y/n)   ")
+
+	if keepAll == "n":
+		for fi in fiFo:
+			delFile = "X"
+			while delFile != "y" and delFile != "n":
+				delFile = input("Would you like to delete {}? (y/n)\t".format(fi))
+			if delFile == "y":
+				final = "X"
+				while final != "y" and final != "n":
+					final = input("(Safe Delete) Are you sure you would like to delete {}? (y/n)\t".format(fi))
+				if final == "y":
+					# Remove from list AND dir
+					if os.path.exists(fi):
+  						os.remove(fi)
+					fiFo.remove(fi)
+	
+	# Don't check these files again
+	for i in fiFo:
+		dontCheck.add(i)
+	
+
 def checkDup(checkDir, dirdir):
+	print()
 	print("Checking directory {}".format(checkDir))
 
 	# Walk through path and check if there are any duplicates
@@ -15,7 +51,7 @@ def checkDup(checkDir, dirdir):
 		# For dirpath, dirname, filenames in directory
 		for dp, dn, fn in os.walk(checkDir):
 			# For file in filenames
-    		for f in fn:
+			for f in fn:
 				f.replace(" ","\ ")
 				pathF = dp + "/" + f
 				for dirpath, dirnames, filenames in os.walk(checkDir):
@@ -26,43 +62,26 @@ def checkDup(checkDir, dirdir):
 	else:
 		# Only the files directly in the directory
 		for files in os.listdir(checkDir):
-			files.replace(" ","\ ")
+			fiFound = []
+			files1 = files.replace(" ","\\ ")
 			pathF = checkDir + "/" + files
-			# Check files with themselves
-			for selif in os.listdir(checkDir):
-				selif.replace(" ", "\ ")
-				pathD = checkDir + "/" + selif
-	
-
-	# If directory found, check for duplicates
-	fiFound = ["WD","As", "Pee"]
-
-	# If duplicates found, notify that duplicates have been found
-	print("Duplicates found for {}.".format(fiFound))
-
-	# Display all the duplicates of that file in form (1: ... \n 2: ... etc.)
-	for key, fi in enumerate(fiFound):
-		print("{}. {}".format(key + 1, fi))
-
-	# Prompt if they would like to keep all files
-	keepAll = "X"
-	while keepAll != "y" and keepAll != "n":
-		keepAll = input("Would you like to keep all the above files? (y/n)   ")
-
-	if keepAll == "n":
-		for fi in fiFound:
-			delFile = "X"
-			while delFile != "y" and delFile != "n":
-				delFile = input("Would you like to delete {}? (y/n)\t".format(fi))
-			if delFile = "y":
-				final = input("(Safe Delete) Are you sure you would like to delete {}? (y/n)\t".format(fi))
-					if final = "y":
-						# Remove from list AND dir
-						fiFound.remove(fi)
-	
-	# Don't check these files again
-	dontCheck = dontCheck + fiFound
-
+			pathF1 = checkDir + "/" + files1
+			if os.path.isfile(pathF):
+				if pathF in dontCheck:
+					continue
+				# Check files with themselves
+				for selif in os.listdir(checkDir):
+					selif1 = selif.replace(" ", "\\ ")
+					pathD = checkDir + "/" + selif
+					pathD1 = checkDir + "/" + selif1
+					cmd = "diff {} {}".format(pathF1,pathD1)
+					if os.path.isfile(pathD):
+						k = subprocess.call(cmd, shell=True, stdout = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
+						if k == 0:
+							fiFound.append(pathD)
+			
+				if len(fiFound) > 1:
+					checkPrompt(fiFound, pathF)
 	print()
 
 
@@ -70,7 +89,7 @@ print("Duplicate Remover v1.0")
 x = "X"
 while x != "y" and x != "n":
 	x = input("Would you like to check directories within directories? (y/n)   ")
-if x == "y"
+if x == "y":
 	dirdir = True
 else:
 	dirdir = False
