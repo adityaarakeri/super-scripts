8e9e27eba4ad8dd92b1f9416bdd670fc80b8e312
Artyom Novoselov <novoselov_artyom@tutanota.com>
2020-10-04 22:50:26 +0300

    added encrypt/decrypt functions

--- xor_encrypt_decrypt_txt/README.md
+++ xor_encrypt_decrypt_txt/README.md
@@ -14,6 +14,7 @@ Make sure to use Python 3. After program start choose an action you want to make
 
 ```
 python xor_encrypt_decrypt.py 
-Choose a mode (e(ncrypt)/d(ecrypt)): 
+Choose a mode e(ncrypt)/d(ecrypt): 
 Enter file's directory: 
+Enter a KEY:
 ```
--- xor_encrypt_decrypt_txt/xor_encrypt_decrypt.py
+++ xor_encrypt_decrypt_txt/xor_encrypt_decrypt.py
@@ -1,2 +1,29 @@
-import os
+def XOR_cipher(string, key):
+    encrypted = ''
+
+    for i in range(len(string)):
+        encrypted += chr(ord(string[i]) ^ ord(key[i % len(key)]))
+
+    return encrypted
+
+
+def XOR_uncipher(string, key):
+    decrypted = ''
+
+    for i in range(len(string)):
+        decrypted += chr(ord(string[i]) ^ ord(key[i % len(key)]))
+    
+    return decrypted
+
+def read_input():
+    fine = False
+    while not fine:
+        print("Choose a mode e(ncrypt)/d(ecrypt): ")
+        mode = input()
+
+        print("Enter file's directory: ")
+        path = input()
+
+        print("Enter a KEY: ")
+        key = input()
 
