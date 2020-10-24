1871339460e91f34b7f77896fe9afeab9800faf7
Afi Maame Dufie <afimaamedufie@gmail.com>
2020-10-24 17:18:27 +0000

    Added a temperature converter

---
+++ temp_converter/README.md
@@ -0,0 +1,11 @@
+# Temperature Converter
+
+# What it does
+This script:
+- converts temperature in Celsius to Fahrenheit. 
+
+# Run
+```
+python3 temp_converter.py
+
+```
---
+++ temp_converter/temp_converter.py
@@ -0,0 +1,12 @@
+#usr/bin/env python
+
+# Prompt for getting the Celsius value
+
+Cels = int(input('Kindly enter a temperature in Celsius: '))
+
+# Conversion to Fahrenheit
+
+Fahr = 9.0 / 5.0 * Cels + 32
+
+# Print statement for output
+print ('You entered: ', Cels,  '\nIts conversion to Fahrenheit is = ', Fahr, "F")
